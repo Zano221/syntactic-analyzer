3479b5559c2196f292ba02a107d02a0d5fd16b9c
Zano221 <zanopcwr@protonmail.com>
2024-06-17 15:16:43 -0300

    it  works, now just need tome refacto and stuff

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
 
-input = null;
 HTMLTable = null;
-table = {
-  "S": "NOT IMPLEMENTED",
-  "A": "NOT IMPLEMENTED",
-  "B": "NOT IMPLEMENTED",
-  "C": "NOT IMPLEMENTED",
+
+var stack = "S";
+var input = null;
+
+let STATE = "S";
+var stepAmmount = 0;
+
+/*S ::= cBb | bCa | abC
+A ::= aCb | cB | bB
+B ::= acC | ε
+C ::= cb | bcS */
+
+/*parsingTable = {
+  "S": { "c": ["c", "B", "b"], "b": ["b", "C", "a"], "a": ["a", "b", "C"] },
+  "A": { "a": ["a", "C", "b"], "c": ["c", "B"], "b": ["b", "B"] },
+  "B": { "a": ["a", "c", "C"], "c": ["a", "c", "C"], "$": ["ε"], "b": ["ε"] },
+  "C": { "c": ["c", "b"], "b": ["b", "c", "S"] } 
+}*/
+
+parsingTableStep = {
+  "S": { "c": "B", "b": "C", "a": "C"},
+  "A": { "a": "C", "c": "B", "b": ["b", "B"] },
+  "B": { "a": ["a", "c", "A"], "c": ["a", "c", "A"], "b": [], "$": ["ε"] },
+  "C": { "c": ["c", "b"], "b": ["b", "c", "S"] }
+}
+
+parsingTable = {
+  "S": { "a": ["a", "A", "c"], "b": ["b", "C", "a"] },
+  "A": { "a": ["a", "B", "a"], "b": ["b", "C", "b"], "c": ["c", "S", "b"] },
+  "B": { "b": ["b", "A", "a"], "c": ["c", "S"] },
+  "C": { "c": ["c", "S", "c"], "$": ["ε"], "a": ["ε"], "b": ["ε"] }
+};
+
+function getProduction(STATE, char) {
+  return parsingTable[STATE][char].reverse().toString().replaceAll(",", "");
+}
+
+function isUpperCase(char) {
+  return /^[A-Z]+$/.test(char)
+}
+
+function stackPop() {
+  return stack.slice(0, stack.length - 1);
 }
 
 function genResultsTable() {
@@ -43,20 +80,126 @@ function resetResultsTable() {
 }
 
 function step() {
-  $("#table-body").append(`<tr id="step=${step}">
-                          </tr>`)
+  /*$("#table-body").append(`<tr id="step=${step}">
+                          </tr>`)*/
   
                           
-  $(`#step=${step}`).append(`<td class="table-terminal-head">`);
+  //$(`#step=${step}`).append(`<td class="table-terminal-head">`);
+  
+  if(parsingTable[STATE][input[0]] == undefined) {
+    console.log("\n\n\n ERROR ME IS UNDEFINED LOL \n\n\n")
+    return
+  }
+
+  if(input.length < 0 && stack.length < 0) return
+
+  console.log(stack, input)
+  let production;
+  //TESTAR UPPERCASE
+  if (stack && stack.length > 0 && isUpperCase(stack[stack.length - 1])) {
+    STATE = stack[stack.length - 1];
+    production = getProduction(STATE, input[0]);
+    stack = stackPop()
+    stack += production;
+  }
+  // se n for uppercase, testar se o topo da pilha e o primeiro da entrada
+  else if (stack && stack.length > 0 && stack[stack.length - 1] === input[0]) {
+    // Le a produção
+    stack = stackPop();
+    input = input.slice(1);
+  }
+  else {
+    console.log("\n\n\n ERRO EM SLA ITERAÇÕES \n\n\n");
+    finished = true;
+    return;
+  }
+  
 }
 
 function EXECUTE() {
   //$("#results").css("visibility", "visible");
   const table = resetResultsTable();
 
+  let finished = false
+
+  input = $("#insert-input").val().split('');
+  if(input.length < 1) return;
 
 
-  console.log("NOT IMPLEMENTED")
+  let STATE = "S";
+  let stepAmmount = 0;
+  let action = "";
+  //PASSOS
+  for(let i = 0; i < 10; i++) {
+    
+
+    if(input.length == 0 && stack.length == 0) {
+      console.log("ACEITO EM 12 BILHOES DE ITERASAO");
+      return;
+    }
+
+    if(parsingTable[STATE][input[0]] == undefined) {
+      console.log("\n\n\n ERROR ME IS UNDEFINED LOL \n\n\n")
+      return
+    }
+    
+    //console.log(stack, input)
+    let production;
+    //TESTAR UPPERCASE
+    if (stack && stack.length > 0 && isUpperCase(stack[stack.length - 1])) {
+      STATE = stack[stack.length - 1];
+      action = `${STATE} -> ${parsingTable[STATE][input[0]]}`
+      production = parsingTable[STATE][input[0]].reverse().toString().replaceAll(",", "");
+      stack = stackPop()
+      if(production !== "ε") {
+        stack += production;
+      }
+    }
+    // se n for uppercase, testar se o topo da pilha e o primeiro da entrada
+    else if (stack && stack.length > 0 && stack[stack.length - 1] === input[0] && !isUpperCase(stack[stack.length - 1])) {
+      // Le a produção
+      action = `Ler ${input[0]}`
+      stack = stackPop()
+      input = input.slice(1);
+    }
+    else {
+      console.log(`\n\n\n ERRO EM SLA ITERAÇÕES \n\n\n ${input.length}, ${stack.length}`);
+      finished = true;
+      return;
+    }
+
+    console.log(`PILHA = ${stack}, ENTRADA = ${input}, AÇÃO = ${action}`)	
+
+
+    
+
+    /*if(isUpperCase(stack[stack.length - 1])) {
+      production = parsingTable[STEP][input[0]].reverse().toString().replaceAll(",", "")
+      stack.slice(0, stack.length-1);
+      stack += production
+    }
+    // se n for uppercase, testar se o topo da pilha e o primeiro da entrada
+    else if(stack[stack.length - 1] == input[0]) {
+      stack = stack.slice(0, stack.length-1);
+      input = input.slice(1);
+    }
+    else {
+      console.log("\n\n\n ERROR TEM LETRA ERRADA AI NO TOPO \n\n\n")
+      finished = true;
+      return
+    }*/ 
+    
+    
+
+   // console.log(production)
+
+    //finished = true
+    stepAmmount++;  
+    //return;
+    finished = true
+
+  }
+
 }
 
 function GENERATE() {
@@ -67,7 +210,7 @@ function GENERATE() {
 
 function GENERATE_STEP() {
   const table = resetResultsTable();
-  
+  console.log(parsingTable["S"]["a"]);
     console.log("NOT IMPLEMENTED")
 } 
 
@@ -76,7 +219,7 @@ function GENERATE_STEP() {
 //MAIN
 $(function(){
 
-  input = $("#insert-input");
+  //input = $("#insert-input").val();
   
   $('#execute-button').click(function() {
     EXECUTE();
